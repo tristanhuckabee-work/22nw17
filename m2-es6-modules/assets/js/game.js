483ec5f4ebe31e617b2a20cfa7d5d6b2c64c7d5c
tristan <thuckabee.work@gmail.com>
2023-03-14 18:52:00 -0500

    m2 done

--- m2-es6-modules/assets/js/game.js
+++ m2-es6-modules/assets/js/game.js
@@ -1,7 +1,9 @@
-class Game {
+import mrsPotatoHeadQuotes from './quotes/mrsPotatoHead.js';
+import {mrPotatoHeadQuotes} from './quotes/mrPotatoHead.js';
+import {getIndex} from "./utilities.js";
 
+export default class Game {
   start() {
-
     document.getElementById('hello').addEventListener('click', () => {
       const index = getIndex();
       const messageContainer = document.getElementById('message');
